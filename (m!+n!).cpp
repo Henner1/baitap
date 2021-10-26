e5c925f8c85dbce0b7577628062f520834666713
Henner1 <92778854+Henner1@users.noreply.github.com>
2021-10-26 16:04:49 +0700

    Add files via upload

---
+++ (m!+n!).cpp
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+int main()
+{
+	int n;
+	float p = 1;
+	do
+	{
+		printf("\nNhập vào số n: ");
+		scanf("%d", &n);
+		if (n < 1)
+		{
+			printf("\nSố n phải lớn hơn hoặc bằng 1, vui lòng nhập lại !");
+		}
+	} while (n < 1);
+	//tính biểu thức bằng vòng lặp for
+	for (int i = 1; i <= n; i++) {
+		p = p * i;
+	}
+
+	printf("\nTích 1 x 2 x ... x %d là: %.1f", n, p);
+
+	printf("\n-----------------------------------------------------\n");
+	printf("Chương trình này được đăng tại Freetuts.net");
+}
---
+++ 1 den k.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int k = 0;
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		k += i;
+	cout << "Tong cac so tu 1 den " << n << " la:  " << k << "\n";
+
+}
