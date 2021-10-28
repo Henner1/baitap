7c5f8a5704e5aaff4cffc6fb31f1b0abf2b11b42
Henner1 <92778854+Henner1@users.noreply.github.com>
2021-10-28 17:57:26 +0700

    Add files via upload

---
+++ tong nhieu giai thua.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+    int sum = 0;
+
+	cout << "Co bao nhieu so: ";
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+
+		int number;
+		cout << "Enter number " << i + 1 << ": ";
+		cin >> number;
+		float p = 1;
+		for (int j = 1; j <= number; j++)
+
+		 p = p * j;
+		sum = sum + p;
+		
+	}
+	
+	cout << "tong : " << sum << endl;
+	return 0;
+
+}
+
