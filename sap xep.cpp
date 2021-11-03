61542b88b4bf84d22314300014310e9bc393f44a
Henner1 <92778854+Henner1@users.noreply.github.com>
2021-11-03 23:09:18 +0700

    Update sap xep.cpp

--- sap xep.cpp
+++ sap xep.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 void sap_xep (int number[], int count)
 {
@@ -21,23 +21,24 @@ void sap_xep (int number[], int count)
     cout << "--------------------------------\n";
     cout << "Chon: ";
     cin >> x;
-    cout << "**********************************\n";
+    cout << "--------------------------------\n";
     if (x == 1)
     {
         
         cout << "Cac so sau khi duoc sap xep tang dan: ";
         for (i = 0; i < count; ++i)
-            cout << "\n" << number[i];
+        cout << "\n" << "Enter number " << i + 1 << ": "<< number[i];
     }
     else if (x==2)
     {
-        cout << "Toi khong biet lam dau";
+        cout << "************************\n";
+        cout << "*Toi khong biet lam dau*\n";
+        cout << "************************\n";
     }
     else
     {
         cout << "****************\n";
         cout << "*Nhap 1 di pls *\n";
-        cout << "*              *\n";
         cout << "****************";
     }
 
@@ -47,14 +48,14 @@ int main()
 {
     int i, count, number[20];
     int x;
-    cout << "Nhap so luong phan tu trong mang: ";
+    cout << "Co bao nhieu so: ";
     cin >> count;
-    cout << "--------------------------------";
-    cout << "\nNhap gia tri cho tung phan tu trong mang:\n";
     cout << "--------------------------------\n";
-    
+    cout << "Cac so do la:\n";
+    cout << "--------------------------------\n";
+
         for (i = 0; i < count; ++i)
-            cin >> number[i];
+         cin >> number[i];
         cout << "--------------------------------\n";
         sap_xep(number, count);
    
